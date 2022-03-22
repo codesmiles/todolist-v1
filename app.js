dc6e9bfdb2ec1732d3ffb6c0c41c8fa9614613ab
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-03-22 22:08:10 +0100

    a new commit

--- app.js
+++ app.js
@@ -2,58 +2,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
+// Set up view engine middleware for ejs 
 app.set("view engine", "ejs");
 
 // middleware to use bodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// (middleware) to be able to access ayour static folder
-app.use(express.static(`public`)); 
+// (middleware) to be able to access your static files in public folder
+// app.use(express.static(`public`)); 
 
-// HOME ROUTE
+// GET HOME ROUTE
 app.get('/', (req, res) => {
-    // res.sendFile(__dirname + '/index.html');
+    // res.sendFile(path.join(__dirname, 'index.html'));
 
     const date = new Date();
     const getday = date.getDay();
-    day = "";
-    
-    switch (getday) { 
-        case 0: day = `Sunday`;
-            break;
-        case 1: day = `Monday`;
-            break;
-        case 2: day = `Tuesday`;
-            break;
-        case 3: day = `Wednesday`;
-            break;
-        case 4: day = `Thursday`;
-            break;
-        case 5: day = `Friday`;
-            break;
-        case 6: day = `Saturday`;
-            break;
-        default: day = `the day is not found`;
-    }
-    // if (getday === 0 || getday === 6) { 
-    //     `${day} - it's weekend`;
-    //     // res.render("list", { kindOfDay: day });
-    //     // res.send(`<h1>its the weekend yall</h1>`);
-        
 
-    // }
-    // else {
-    //     `${day} - it's weekday`;
-       
-    //     // res.send(`<h1>its the weekday</h1>`);
-    // }
-    
+    // date method to get the day of the week cleanly
+    let options = {
+        weekday: `long`,
+        day: `numeric`,
+        month: `long`,
+    };
+
+    let day = date.toLocaleDateString("en-US", options);
+
+    // ejs render to views/list.ejs
     res.render("list", { kindOfDay: day });
 });
 
+// POST HOME ROUTE
+app.post(`/`, (req, res) => { 
+    const list = req.body.newItem;
+    // const item = `<li>${list}</li>`;
+    console.log(list);
+});
+
+// GET ABOUT ROUTE
 app.get(`/about`, (req, res) => {
 
     res.send(`<h1>About Page</h1>`);
