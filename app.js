8ecdbf27e0c8069b4cdec8603cf65c8d2aff951c
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-06-03 15:25:34 +0100

    recent update

--- app.js
+++ app.js
@@ -3,12 +3,49 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const date = require(__dirname + "/date.js");
-
 const app = express();
 
-const items = [`buy food`, `cook food`, `eat food`];
-const workItems = [];
+// const items = [`buy food`, `cook food`, `eat food`];
+// const workItems = [];
 
+// --------------------------------------------------
+// setting up mongoose
+const mongoose = require("mongoose");
+const url = "mongodb://localhost:27017/todolistDB";
+mongoose.connect(url, { useNewUrlParser: true });
+
+// schema
+const itemsSchema = {
+  name: {
+    type: String,
+  },
+};
+
+// model
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: `welcome to your todolist`,
+});
+
+const item2 = new Item({
+  name: `hit + to add new item`,
+});
+const item3 = new Item({
+  name: `<-------hit this to delete an item`,
+});
+
+const defaultItems = [item1, item2, item3];
+
+// routing param schema
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+};
+// model
+const List = mongoose.model("List", listSchema);
+
+// -------------------------------------------------------------
 // Set up view engine middleware for ejs
 app.set("view engine", "ejs");
 
@@ -20,35 +57,74 @@ app.use(express.static(`public`));
 
 // GET HOME ROUTE
 app.get("/", (req, res) => {
-  // res.sendFile(path.join(__dirname, 'index.html'));
-
-  let day = date();
-
-  // ejs render to views/list.ejs
-  res.render("list", { listTitle: day, newListItems: items });
+  // to check if items exists in the database
+  Item.find({}, (err, foundItems) => {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        err
+          ? console.log(`error ${err}`)
+          : console.log("successfully added items to the database");
+      });
+      res.redirect("/");
+    } else {
+      // ejs render to views/list.ejs
+      let day = date();
+      res.render("list", { listTitle: day, newListItems: foundItems });
+      // mongoose.connection.close();
+    }
+  });
 });
 
-// POST HOME ROUTE
+// GET DYNAMIC ROUTES
+app.get(`/:customListName`, (req, res) => {
+  const listParameter = req.params.customListName;
+
+  // find if a list already exists in the database
+  List.findOne({ name: listParameter }, (err, foundList) => {
+    if (!err) {
+      if (!foundList) {
+        // create a new list
+        const list = List({
+          name: listParameter,
+          items: defaultItems,
+        });
+
+        list.save();
+      } else {
+        // show the existing list
+        res.render(`list`, {
+          listTitle: foundList.name,
+          newListItems: foundList.items,
+        });
+      }
+    }
+  });
+});
+
+// add new items
 app.post(`/`, (req, res) => {
-  const list = req.body.newItem;
-  // to check if the input tag is empty
-  if (list === "") {
+  const itemName = req.body.newItem;
+  // // to check if the input tag is empty
+  if (itemName === "") {
     res.redirect("/");
   } else {
-    
-    if (req.body.list === `Work List`) {
-      workItems.push(list);
-      res.redirect(`/work`);
-    } else {
-      items.push(list);
-      res.redirect(`/`);
-    }
+    const item = new Item({
+      name: itemName,
+    });
+    item.save();
+    res.redirect("/");
   }
 });
 
-// GET WORK ROUTE
-app.get(`/work`, (req, res) => {
-  res.render(`list`, { listTitle: `Work List`, newListItems: workItems });
+// delete item from the list
+app.post(`/delete`, (req, res) => {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, function (err) {
+    err
+      ? console.log(`error ${err}`)
+      : console.log("successfully deleted an item");
+  });
+  res.redirect("/");
 });
 
 // POST WORK ROUTE
@@ -57,7 +133,7 @@ app.post`/work`,
     const items = req.body.newItem;
     // console.log(list);
 
-    workItems.push(items);
+    // workItems.push(items);
 
     res.redirect(`/work`);
   };
